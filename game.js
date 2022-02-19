4e369cd5306f1435d24c59e71cc1ea6cc9b0de05
kgsensei <jt@kellem.com>
2022-02-18 22:12:57 -0500

    Second Commit

--- game.js
+++ game.js
@@ -1,11 +1,36 @@
 pies=0
 piesPerClick=1
 piesPerSecond=0
+settingsMute=false
+pieClickAnimationId=0
 
 $(document).ready(()=>{
 	loadGame()
 	refreshGame()
 })
+
+$("#pie").click((e)=>{
+	$("#pie").stop(true,false)
+	$("#pie").css({"width":"90%","left":"5%","top":"15%"})
+	pieClickAnimationId=pieClickAnimationId+1;
+	$("body").append('<div id="pieInd' + pieClickAnimationId + '" hidden style="pointer-events:none;">+' + piesPerClick + '</div>');
+	$("#pieInd" + pieClickAnimationId).css("top", (e.pageY + getRndInteger(-8,8)) + "px");
+	$("#pieInd" + pieClickAnimationId).css("left", (e.pageX + getRndInteger(-8,8)) + "px");
+	$("#pieInd" + pieClickAnimationId).css("position", "absolute");
+	$("#pieInd" + pieClickAnimationId).css("font-size", "20px");
+	$("#pieInd" + pieClickAnimationId).css("color", "white");
+	$("#pieInd" + pieClickAnimationId).css("animation", "GoUp 2s forwards linear");
+	$("#pieInd" + pieClickAnimationId).show();
+	pies=pies+piesPerClick
+	saveGame()
+	refreshGame()
+	$("#pie").animate({"width":"95%","left":"2.5%","top":"14%"},50)
+	$("#pie").animate({"width":"90%","left":"5%","top":"15%"},100)
+})
+
+function getRndInteger(min,max){
+	return Math.floor(Math.random()*(max-min+1))+min
+}
 
 function saveGame(){
 	localStorage.setItem("pies",pies);
@@ -27,8 +52,54 @@ function refreshGame(){
 	$("#pieGRate").html("Pies/Sec: " + piesPerSecond)
 }
 
-function pieClick(){
-	pies=pies+piesPerClick
-	saveGame()
-	refreshGame()
+function sleep(ms){
+	return new Promise(resolve=>setTimeout(resolve,ms))
+}
+
+async function PleasePlayTheCredits(){
+	settingsMute=true
+	$("#creditsOverlay").fadeIn(1000)
+	await sleep(500)
+	$("#creditsName").html("kgsensei")
+	$("#creditsTitle").html("Lead Programmer")
+	$("#creditsName").fadeIn(500)
+	$("#creditsTitle").fadeIn(500)
+	await sleep(3000)
+	$("#creditsName").fadeOut(500)
+	$("#creditsTitle").fadeOut(500)
+	await sleep(500)
+	$("#creditsName").html("Azuriah Jerez")
+	$("#creditsTitle").html("Graphic Designer")
+	$("#creditsName").fadeIn(500)
+	$("#creditsTitle").fadeIn(500)
+	await sleep(3000)
+	$("#creditsName").fadeOut(500)
+	$("#creditsTitle").fadeOut(500)
+	await sleep(500)
+	$("#creditsName").html("Caleb Rhinehart")
+	$("#creditsTitle").html("Concept Builder")
+	$("#creditsName").fadeIn(500)
+	$("#creditsTitle").fadeIn(500)
+	await sleep(3000)
+	$("#creditsName").fadeOut(500)
+	$("#creditsTitle").fadeOut(500)
+	await sleep(500)
+	$("#creditsName").html("Kylea Reed")
+	$("#creditsTitle").html("Concept Builder")
+	$("#creditsName").fadeIn(500)
+	$("#creditsTitle").fadeIn(500)
+	await sleep(3000)
+	$("#creditsName").fadeOut(500)
+	$("#creditsTitle").fadeOut(500)
+	await sleep(500)
+	$("#creditsName").html("CopyPaste Games")
+	$("#creditsTitle").html("Copyright &copy; 2022")
+	$("#creditsName").fadeIn(500)
+	$("#creditsTitle").fadeIn(500)
+	await sleep(3000)
+	$("#creditsName").fadeOut(500)
+	$("#creditsTitle").fadeOut(500)
+	await sleep(750)
+	$("#creditsOverlay").fadeOut(1000)
+	settingsMute=false
 }
